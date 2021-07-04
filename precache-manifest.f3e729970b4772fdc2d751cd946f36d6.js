d391fc7751181ab0f8cc7f1efed75cd072bfcfa3
unknown <mehrdadzare2003@gmail.com>
2021-07-04 17:10:51 +0430

    Updates

---
+++ precache-manifest.f3e729970b4772fdc2d751cd946f36d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0fd06a982ec4ae74cf3dd8e9e5d028",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c97b5437e331a9651b3d",
+    "url": "/my-app/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/my-app/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c97b5437e331a9651b3d",
+    "url": "/my-app/static/js/main.32f1ffba.chunk.js"
+  },
+  {
+    "revision": "7532727beacde361782c",
+    "url": "/my-app/static/js/runtime-main.59a0bd51.js"
+  }
+]);
